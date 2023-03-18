fd1da5e6421538370aba99ae631f7d76bd226bcf
aucaland <aurelien.calandrino@outlook.fr>
2023-03-18 13:52:35 +0100

    ex04 CPP01 ok

---
+++ CPP-01/ex04/incs/outputs.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   outputs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aurel <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/18 13:36:04 by aurel             #+#    #+#             */
+/*   Updated: 2023/03/18 13:50:53 by aurel            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#ifndef CPP_OUTPUTH_H
+#define CPP_OUTPUTH_H
+
+#define INV_ARG			"3 args : [filename] [string_to_replace] " \
+"[replacement_string] ==> filename and s1 must not be empty string"
+#define OPEN_ERROR		"Open file error"
+#define CLOSE_ERROR		"Close file error"
+#endif //CPP_OUTPUTH_H
---
+++ CPP-01/ex04/srcs/main.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aurel <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/18 12:43:32 by aurel             #+#    #+#             */
+/*   Updated: 2023/03/18 13:48:52 by aurel            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "../incs/outputs.h"
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::flush;
+
+int main(int ac, char **av)
+{
+	string filename;
+	string line;
+	string s1;
+	string s2;
+
+	if (ac != 4)
+	{
+		cout << INV_ARG << endl;
+		return (0);
+	}
+	filename = av[1];
+	s1 = av[2];
+	s2 = av[3];
+	if (s1.empty() || filename.empty())
+	{
+		cout << INV_ARG << endl;
+		return (0);
+	}
+	std::ifstream input_file(filename.c_str());
+	if (input_file.fail())
+	{
+		std::cerr << OPEN_ERROR << endl;
+		return (0);
+	}
+	std::ofstream output_file(string(filename + ".replace").c_str());
+	if (output_file.fail())
+	{
+		std::cerr << OPEN_ERROR << endl;
+		return (0);
+	}
+	while (getline(input_file, line))
+	{
+
+		size_t pos = line.find(s1);
+		while (pos != string::npos)
+		{
+			output_file << line.substr(0, pos) << s2;
+			line = line.substr(pos + s1.length());
+			pos = line.find(s1);
+		}
+		output_file << line << endl;
+	}
+	input_file.close();
+	output_file.close();
+}
