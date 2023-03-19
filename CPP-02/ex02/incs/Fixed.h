bf8af6d2e8017851962f2012cae2dbee540a5030
aucaland <aurelien.calandrino@outlook.fr>
2023-03-19 14:58:24 +0100

    ex02 cpp 02 ok

---
+++ CPP-02/ex02/incs/Fixed.h
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aurel <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/18 16:46:13 by aurel             #+#    #+#             */
+/*   Updated: 2023/03/19 14:56:55 by aurel            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CPP_FIXED_H
+#define CPP_FIXED_H
+
+#include <iostream>
+
+class Fixed
+{
+public :
+	Fixed();
+	~Fixed();
+	Fixed( const int x );
+	Fixed( const float y );
+	Fixed(const Fixed &other);
+	Fixed& operator=(const Fixed &other);
+	bool operator>(const Fixed &other) const;
+	bool operator<(const Fixed &other) const;
+	bool operator>=(const Fixed &other) const;
+	bool operator<=(const Fixed &other) const;
+	bool operator==(const Fixed &other) const;
+	bool operator!=(const Fixed &other) const;
+
+	Fixed operator+(const Fixed &other) const;
+	Fixed operator-(const Fixed &other) const;
+	Fixed operator*(const Fixed &other) const;
+	Fixed operator/(const Fixed &other) const;
+
+	Fixed& operator++( void );
+	Fixed operator ++( int );
+	Fixed& operator--( void );
+	Fixed operator --( int );
+
+
+
+	float		toFloat( void ) const;
+	int			toInt( void ) const;
+	int			getRawBits( void ) const;
+	void		setRawBits( int const raw);
+	static Fixed&	min(Fixed& fixedOne, Fixed& fixedTwo);
+	static const Fixed&	min(const Fixed& fixedOne, const Fixed& fixedTwo);
+	static Fixed&	max(Fixed& fixedOne, Fixed& fixedTwo);
+	static const Fixed&	max(const Fixed& fixedOne, const Fixed& fixedTwo);
+
+private :
+	static const int	_nbrFracBit;
+	int					_fixedPoint;
+};
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
+#endif //CPP_FIXED_H
---
+++ CPP-02/ex02/srcs/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aurel <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/18 16:57:53 by aurel             #+#    #+#             */
+/*   Updated: 2023/03/18 19:32:13 by aurel            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/Fixed.h"
+#include <iostream>
+int main()
+{
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}
